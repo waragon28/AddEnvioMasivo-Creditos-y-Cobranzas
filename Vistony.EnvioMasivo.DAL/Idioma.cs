c8c2ce850b82693cd4c85a65a214965388f56e91
waragon28 <waragon@nogasa.com.pe>
2023-03-01 11:18:06 -0500

    Agregar Add-Ons

---
+++ Vistony.EnvioMasivo.DAL/Idioma.cs
@@ -0,0 +1,24 @@
+﻿using Forxap.Framework.ServiceLayer;
+using SAPbobsCOM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vistony.Masivo.DAL
+{
+    public class Idioma
+    {
+        public string GetIdiomaSAP(Recordset recordset,string UserLoging)
+        {
+            string StrHANA = string.Empty;
+            StrHANA = string.Format("SELECT FX_LENGUAGE_SAP(TO_INT(\"Language\")) AS \"Idioma\" "+
+                                    "FROM \"OUSR\" where \"USER_CODE\" = '{0}'", UserLoging);
+
+            recordset.DoQuery(StrHANA);
+            string Idioma = recordset.Fields.Item("Idioma").Value.ToString();
+            return  Idioma;
+        }
+    }
+}
---
+++ Vistony.EnvioMasivo.Win/ApplicationEvents.cs
@@ -0,0 +1,60 @@
+﻿using Forxap.Framework.ServiceLayer;
+using SAPbobsCOM;
+using SAPbouiCOM.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vistony.Masivo.DAL;
+
+namespace Vistony.Distribucion.Win
+{
+    public class ApplicationEvent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="EventType"></param>
+
+        Idioma GetIdioma = new Idioma();
+        public void SB1_Application_AppEvent(SAPbouiCOM.BoAppEventTypes EventType)
+        {
+            switch (EventType)
+            {
+                case SAPbouiCOM.BoAppEventTypes.aet_ShutDown:
+                    //Exit Add-On
+                    System.Windows.Forms.Application.Exit();
+                    break;
+                case SAPbouiCOM.BoAppEventTypes.aet_CompanyChanged:
+                    // deberia inicializar el addon quizas???? 
+                    break;
+                case SAPbouiCOM.BoAppEventTypes.aet_FontChanged:
+                    break;
+                case SAPbouiCOM.BoAppEventTypes.aet_LanguageChanged:
+                    SAPbobsCOM.Recordset oRS = (SAPbobsCOM.Recordset)Sb1Globals.oCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+                
+                    Sb1Globals.Idioma = GetIdioma.GetIdiomaSAP(oRS, Sb1Globals.UserName);
+                    break;
+                case SAPbouiCOM.BoAppEventTypes.aet_ServerTerminition:
+                    break;
+
+                    /*IDIOMA*/
+               // case SAPbouiCOM.BoAppEventTypes.aet_LanguageChanged:
+                   // log.Info("aet_LanguageChanged - init addin");
+                    
+                  //  Globals.CultureInfo = GetCulture(Application.Language)
+                   //     break;
+
+
+                default:
+                    break;
+            }
+        }
+
+
+
+
+    }// fin de la clase
+
+}// fin del namespace
